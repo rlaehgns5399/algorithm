c8e3ed983723a6aa875b26d6f589643cb1b5d193
rlaehgns5399 <rlaehgns5398@gmail.com>
2018-09-06 12:24:18 +0900

    Insertion Sort & Merge Sort
    2019.09.04

---
+++ InsertionSort.java
@@ -0,0 +1,23 @@
+
+public class InsertionSort extends PrintArr{
+	private int[] arr;
+	
+	public InsertionSort(int[] arr){
+		super.printArr(arr);
+		int i, j, temp;
+		for(i = 1; i < arr.length; i++){
+			temp = arr[i];
+			for(j = i - 1; j >= 0; j--){
+				if(temp > arr[j]) break;
+				arr[j+1] = arr[j];
+			}
+			arr[j + 1] = temp;
+		}
+		super.printArr(arr);
+		this.arr = arr;
+	}
+	
+	public int[] getArr(){
+		return this.arr;
+	}
+}
---
+++ MergeSort.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeSort extends PrintList{
+	List<Integer> list;
+	public MergeSort(List<Integer> arr){
+		super.printList(arr);
+		list = doMergeSort(arr);
+		super.printList(list);
+	}
+	private List<Integer> doMergeSort(List<Integer> arr){
+		if(arr.size() < 2) return arr;
+		int pivot = (int)(arr.size() / 2);
+		List<Integer> left = arr.subList(0, pivot);
+		List<Integer> right = arr.subList(pivot, arr.size());
+		return Merge(doMergeSort(left), doMergeSort(right));
+	}
+	
+	private List<Integer> Merge(List<Integer> left, List<Integer> right){
+		List<Integer> result = new ArrayList<Integer>();
+		while(left.size() > 0 && right.size() > 0){
+			if(left.get(0) <= right.get(0)){
+				result.add(left.get(0));
+				left = left.subList(1, left.size());
+			} else {
+				result.add(right.get(0));
+				right = right.subList(1, right.size());
+			}
+		}
+		
+		for(int i = 0; i < left.size(); i++){
+			result.add(left.get(i));
+		}
+		for(int i = 0; i < right.size(); i++){
+			result.add(right.get(i));
+		}
+		return result;
+	}
+}
