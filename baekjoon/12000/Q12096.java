e2f6ec6eb34716a5ec12e58bed808bbdb876fc50
Kim Do Hun <rlaehgns5398@gmail.com>
2019-02-05 08:47:01 +0900

    BOJ 7대난제??

---
+++ baekjoon/12000/Q12096.java
@@ -0,0 +1,7 @@
+// 문제가 base64로 인코딩 되어있어, 소스보기를 통해 base64 코드를 얻고, 디코딩 툴로 문제를 해결
+
+public class Main{
+    public static void main(String arg[]){
+        System.out.println("02-521-0487");
+    }
+}
