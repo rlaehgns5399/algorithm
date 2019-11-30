140871a8a11e07c084c34bd6dcc40b913965bc49
Kim Do Hun <rlaehgns5398@gmail.com>
2019-11-30 20:18:58 +0900

    Create LEET_pow(x,n).js

---
+++ JS_BOJ/Math/LEET_pow(x,n).js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    return Math.pow(x, n);
+};
+
+
+// recursion solution
+
+const myPow = (x, n) => {
+  if (n === 0) return 1;
+  if (n === 1) return x;
+  if (n < 0) return 1 / myPow(x, -n);
+  return (n & 1) === 0 ? myPow(x * x, n >>> 1) : x * myPow(x * x, (n - 1) >>> 1);
+};
