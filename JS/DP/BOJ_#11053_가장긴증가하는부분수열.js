bb5771af049de2f47070f9b3d667095b3127cc8b
Kim Do Hun <rlaehgns5398@gmail.com>
2019-12-12 00:02:46 +0900

    Create BOJ_#11053_가장긴증가하는부분수열.js

---
+++ JS/DP/BOJ_#11053_가장긴증가하는부분수열.js
@@ -0,0 +1,23 @@
+let input = require('fs').readFileSync('dev/stdin').toString().split('\n');
+const n = Number(input[0]);
+const array = input[1].split(' ').map(Number);
+array.unshift(0);
+
+const dp = new Array(array.length);
+dp[0] = 0;
+for(let i = 1; i <= n; i++) {
+    let min = 0;
+    for(let j = 0; j < i; j++) {
+        if (array[i] > array[j]) {
+            if (min < dp[j]) min = dp[j];
+        }
+    }
+    dp[i] = min + 1;
+}
+
+let max = 0;
+for(let i = 0; i <= n; i++) {
+    max = Math.max(max, dp[i]);
+}
+
+console.log(max);
