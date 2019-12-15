4a6b089c606e9903d2b670ffb538c1349c20ccb6
Kim Do Hun <rlaehgns5398@gmail.com>
2019-12-15 20:44:28 +0900

    Create BOJ_#11650_좌표정렬하기.js

---
+++ JS/Sort/BOJ_#11650_좌표정렬하기.js
@@ -0,0 +1,20 @@
+const input = require('fs').readFileSync('dev/stdin').toString().split('\n');
+const n = Number(input[0]);
+const array = [];
+
+for(var i = 1; i <= n; i++) {
+    var item = input[i].split(' ').map(Number);
+    array.push([item[0], item[1]]);
+}
+
+let result = '';
+array.sort((a, b) => {
+    if (a[0] !== b[0]) {
+        return a[0] - b[0];
+    } else {
+        return a[1] - b[1];
+    }
+}).forEach((item) => {
+    result += `${item[0]} ${item[1]}\n`;
+});
+console.log(result);
